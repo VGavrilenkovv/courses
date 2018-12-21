55ab571e892019e4f8597b6a349d6d18c673d955
Student <i.domnich2013@gmail.com>
2018-12-21 20:12:17 +0200

    повторить цикл форич и массивы

---
+++ src/lesson5/cicleFor.java
@@ -0,0 +1,14 @@
+package lesson5;
+
+/**
+ * Created by Student on 26.11.2018.
+ */
+public class cicleFor {
+    public static void main(String[] args) {
+        char ch1 = 'h';
+        for (int i = 0; i<5; i ++){
+            System.out.println(ch1++);
+        }
+
+    }
+}
